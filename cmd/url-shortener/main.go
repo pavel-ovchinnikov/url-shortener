2eb5c8ff986545c943bb9cfbd6044776770e4b57
Pavel Ovchinnikov <povchinnikov.life@icloud.com>
2023-08-30 13:25:02 +0300

    update confug

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,33 +2,16 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"net/http"
 
 	"url-shortener/internal/config"
 )
 
-func addLink(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "This is my website!\n")
-	fmt.Println("Hello")
-}
-
-func removeLink(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "This is my website!\n")
-	fmt.Println("Hello")
-}
-
-func patchLink(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "This is my website!\n")
-	fmt.Println("Hello")
-}
-
 func main() {
-	cnf := config.Config{}
-	cnf.Address = ":8090"
+	cfg := config.MustLoad()
+	fmt.Println(cfg)
 
-	http.HandleFunc("/add", addLink)
-	http.HandleFunc("/remove", removeLink)
-	http.HandleFunc("/patch", patchLink)
-	http.ListenAndServe(cnf.HTTPServer.Address, nil)
+	// TODO: init logger: slong
+	// TODO: init storage
+	// TODO: init router: chi
+	// TODO: init server
 }
