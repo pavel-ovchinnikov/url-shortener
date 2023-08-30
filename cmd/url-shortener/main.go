8c1c8a98619617958c8c7f2f7aaf33d9c3acb6aa
Pavel Ovchinnikov <povchinnikov.life@icloud.com>
2023-08-30 15:43:19 +0300

    add storage: sqlite

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
 
 	"url-shortener/internal/config"
+	"url-shortener/internal/storage/sqlite"
 )
 
 func main() {
+
 	cfg := config.MustLoad()
-	fmt.Println(cfg)
+
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Fatalf("failed to init storage %s", err)
+		os.Exit(1)
+	}
+
+	id, err := storage.SaveURL("https:/google.com", "google")
+	if err != nil {
+		log.Fatalf("failed to init storage %s", err)
+	} else {
+		log.Println("id ", id)
+	}
 
 	// TODO: init logger: slong
-	// TODO: init storage
 	// TODO: init router: chi
 	// TODO: init server
 }
