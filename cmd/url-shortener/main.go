30d5c52e7ac88db6723de50230ed96340cdb9915
Pavel Ovchinnikov <povchinnikov.life@icloud.com>
2024-08-06 13:48:02 +0300

    init logger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"log/slog"
+	"os"
+
 	"github.com/pavel-ovchinnikov/url-shortener/internal/config"
 )
 
 func main() {
 	// TODO: init config
 	cfg := config.MustLoad()
+	_ = cfg
 
+	// TODO: init context
 	// TODO: init logger
+	log := NewLogger()
+
 	// TODO: init storage
 	// TODO: init router
 	// TODO: run server
+}
 
+func NewLogger() *slog.Logger {
+	return slog.New(
+		slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+	)
 }
